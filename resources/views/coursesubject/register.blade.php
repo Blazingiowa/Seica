117dcc312961c6bb0b1c512512de8e23a4177566
敷島 <yasutaka913@gmail.com>
2022-09-05 22:23:33 +0900

    整形

--- resources/views/coursesubject/register.blade.php
+++ resources/views/coursesubject/register.blade.php
@@ -3,60 +3,25 @@
 @section('main')
     <div class="row">
         <div class="col-xl-10">
-            <!-- ============================================================== -->
-            <!-- pageheader  -->
-            <!-- ============================================================== -->
-            <div class="row">
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <div class="page-header" id="top">
-                        <h2 class="pageheader-title">Form Elements </h2>
-                        <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
-                        <div class="page-breadcrumb">
-                            <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Dashboard</a></li>
-                                    <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Forms</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Form Elements</li>
-                                </ol>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- ============================================================== -->
-            <!-- end pageheader  -->
-            <!-- ============================================================== -->
             <div class="page-section" id="overview">
-                <!-- ============================================================== -->
-                <!-- overview  -->
-                <!-- ============================================================== -->
                 <div class="row">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                        <h2>Overview</h2>
-                        <p class="lead">Nam pulvinar interdum turpis a mattis. Etiam augue leo, mollis a massa sagittis, egestas pretium risus. Aliquam auctor nibh mauris, at fringilla elit lobortis sodales. Praesent volutpat felis et placerat elementum. </p>
+                        <h2>科目登録</h2>
+                        <p class="lead">授業の科目登録を行います</p>
                         <ul class="list-unstyled arrow">
-                            <li> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                            <li>Mauris bibendum massa ut porttitor congue.</li>
-                            <li>Morbi condimentum magna eget facilisis accumsan.</li>
-                            <li>Proin euismod eros nec libero efficitur, a dapibus mauris condimentum. </li>
+                            <li>今年度の授業科目を入力してください</li>
                         </ul>
                     </div>
                 </div>
-                <!-- ============================================================== -->
-                <!-- end overview  -->
-                <!-- ============================================================== -->
+
             </div>
-            <!-- ============================================================== -->
-            <!-- basic form  -->
-            <!-- ============================================================== -->
+
             <div class="row">
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                     <div class="section-block" id="basicform">
-                        <h3 class="section-title">Basic Form Elements</h3>
-                        <p>Use custom button styles for actions in forms, dialogs, and more with support for multiple sizes, states, and more.</p>
+                        <h3 class="section-title">新規科目登録</h3>
                     </div>
                     <div class="card">
-                        <h5 class="card-header">Basic Form</h5>
                         <div class="card-body">
                             <form>
                                 <div class="form-group">
